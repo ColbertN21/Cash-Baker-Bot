1519771586fb9a03ea55e34975057f7fee448b4f
ColbertN21 <55058574+ColbertN21@users.noreply.github.com>
2020-08-17 01:15:49 -0700

    Update cashbaker.py

--- cashbaker.py
+++ cashbaker.py
@@ -11,14 +11,24 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-
+    msg = message.content.split()
+    for msgcon in msg:
+        if msgcon == "fuck":
+            await message.delete()
+            await message.channel.send("Watch your fucking whore mouth!")
     if message.content.startswith('!cash'):
-        with open("cbblinks.txt") as word_file:
-            words = word_file.read().split()
-        randtext = random.choice(words)
-
-        await message.channel.send(randtext)
-
+        randlink = randline("cbblinks.txt")
+        randi = randline("rvi.txt")
+        await message.channel.send(randi + " " + randlink)
+    if message.content.startswith('!speak'):
+        randphrase = randline("cbrp.txt")
+        await message.channel.send(randphrase)
+    if message.content.startswith('!help'):
+        await message.channel.send("I am the Cash Baker Bot!\nYou can find my source code at https://github.com/ColbertN21/Cash-Baker-Bot \nCurrently my commands are as follows:\n!cash: Returns random tiktok link\n!speak: Returns random phrase\n!help: returns list of commands")
+    
+def randline(file):
+    lines = open(file).read().splitlines()
+    return random.choice(lines)
 
 token = open("apitoken.txt")
 client.run(token.readline())
